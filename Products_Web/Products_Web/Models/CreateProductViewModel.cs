e0aed4b4430b6d9f50055078fe4be658df24be8a
Ilko Trapov <trapovilko@gmail.com>
2024-03-22 20:09:57 +0200

    First

---
+++ Products_Web/Products_Web/Models/CreateProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Products_Web.Models
+{
+    public class CreateProductViewModel
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Products_Web/Products_Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Products_Web.Data.Entities;
+using Products_Web.Models;
+
+namespace Products_Web.Services.Interfaces
+{
+    public interface IProductService
+    {
+        void Add(CreateProductViewModel product);
+
+        //ToDo: Get all
+
+        //ToDo: Create
+
+        //ToDo: Update
+
+        //ToDo: Get by Id
+    }
+}
